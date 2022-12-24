84a55cacfaa2eb7bcc40c81eb63c04e370845a95
טליה ישראל <t0505154143@gmail.com>
2022-12-24 23:51:52 +0200

    tdgd

---
+++ js/game1.js
@@ -0,0 +1,4 @@
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}

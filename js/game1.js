4c4514a4d6dec5c69b3480949b5748694092e90e
טליה ישראל <t0505154143@gmail.com>
2022-12-25 00:25:14 +0200

    hv

--- js/game1.js
+++ js/game1.js
@@ -2,3 +2,11 @@
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
+
+function showImgNumber(){
+    let one= document.getElementById("one");
+    one.src="C:\Users\טליה ישראל\Desktop\לימודים סמסטר א' תשפ'ג\Full Stack\פרוייקט 2\FullStack_project2\media\chess.jpg";
+    let two= document.getElementById("two");
+    two.src="C:\Users\טליה ישראל\Desktop\לימודים סמסטר א' תשפ'ג\Full Stack\פרוייקט 2\FullStack_project2\media\chess.jpg";
+}
+

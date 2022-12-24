9fdeedf01d8fdca95dc8c3caddcf27c800031d99
אודיה סעדון <osadoun@g.jct.ac.il>
2022-12-24 23:12:18 +0200

    localstroge

--- js/games.js
+++ js/games.js
@@ -1,3 +1,12 @@
 function alertComingSoon(){
     alert('בקרוב!!!!');
 }
+
+window.onload = function(){
+    const email = document.cookie.split(';')[0]; //key value 0 = email
+    const emailvalue = email.substring(email.indexOf('=') + 1);
+ let user = localStorage.getItem(emailvalue);
+
+ alert(user);
+}
+

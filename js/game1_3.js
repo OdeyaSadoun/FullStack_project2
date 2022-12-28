cc3f7da69b2f9fc761f113c409118e59eaaeaad3
טליה ישראל <t0505154143@gmail.com>
2022-12-28 13:41:28 +0200

    commit

--- js/game1_3.js
+++ js/game1_3.js
@@ -1,8 +1,13 @@
 function showImgABC(letter) {
-    findLetter(letter);
-  }
+  findLetter(letter);
+}
 
-  function findLetter(l){
-    let letABC= document.getElementById(l.toString());
-    letABC.src ='../media/abc-poto/'+ l.toString() +'.jpg';
-  }
+function findLetter(l){
+  let letABC= document.getElementById(l.toString());
+  letABC.src ='../media/abc-poto/'+ l.toString() +'.jpg';
+}
+
+/* showDropdown toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function showDropdown() {
+  document.getElementById('myDropdown').classList.toggle('show');
+}

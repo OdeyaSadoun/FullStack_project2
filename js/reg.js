31caeb9a48608a032aa83237f2ee16981c7804ed
אודיה סעדון <osadoun@g.jct.ac.il>
2022-12-28 17:50:21 +0200

    add

--- js/reg.js
+++ js/reg.js
@@ -2,59 +2,77 @@ function verifyPassword() {
     var pw = document.getElementById('password').value;
     //check empty password field  
     if (pw == "") {
-        document.getElementById("message").innerHTML = "חסר שדה סיסמא- יש למלא!";
+        document.getElementById("message").innerHTML = "לא הוזנה סיסמא - יש למלא";
+        document.getElementById("password").style.borderColor = "red";
         return false;
     }
 
     //minimum password length validation  
     if (pw.length < 8) {
         document.getElementById("message").innerHTML = "אורך הסיסמא חייב להכיל לפחות 8 תווים";
+        document.getElementById("password").style.borderColor = "red";
         return false;
     }
 
     //maximum length of password validation  
     if (pw.length > 15) {
         document.getElementById("message").innerHTML = "אורך הסיסמא יכול להכיל לא יותר מ15 תווים";
+        document.getElementById("password").style.borderColor = "red";
         return false;
-    } else {
+    }
+    else {
         document.getElementById("message").innerHTML = "הסיסמא תקינה:)";
+        // document.getElementById("password").style.borderColor = "default";
     }
 
     return true;
 }
 
 function verifyName() {
+    //function to verify the text fields
+
     var fname = document.getElementById("first_name").value;
     var lname = document.getElementById("last_name").value;
 
     //check empty first name field  
     if (fname == "") {
         document.getElementById("message").innerHTML = "יש למלא את השם הפרטי";
+        document.getElementById("first_name").style.borderColor = "red";
         return false;
     }
 
     //character data validation  
     if (!isNaN(fname)) {
-        document.getElementById("message").innerHTML = "רק אותיות אפשריות- לא מספרים";
+        document.getElementById("message").innerHTML = "יש להזין אותיות ותווים- לא מספרים";
+        document.getElementById("first_name").value = "";
+        document.getElementById("first_name").style.borderColor = "red";
         return false;
     }
 
     //character data validation  
     if (!isNaN(lname)) {
-        document.getElementById("message").innerHTML = "רק אותיות אפשריות- לא מספרים";
+        document.getElementById("message").innerHTML = "יש להזין אותיות ותווים- לא מספרים";
+        document.getElementById("last_name").value = "";
+        document.getElementById("last_name").style.borderColor = "red";
         return false;
     }
+
     return true;
 }
 
 
 function confirmPassword() {
+    //function to confirm the password
     var pw1 = document.getElementById("password").value;
     var pw2 = document.getElementById("re_password").value;
+
     if (pw1 !== pw2) {
         document.getElementById("message").innerHTML = "הסיסמא ואימות הסיסמא אינן שוות- יש לנסות שוב";
+        document.getElementById("re_password").value = "";
+        document.getElementById("re_password").style.borderColor = "red";
         return false;
-    } else {
+    }
+    else {
         document.getElementById("message").innerHTML = "נפלא!!!";
         return true;
     }
@@ -62,12 +80,6 @@ function confirmPassword() {
 
 
 function submitReg() {
-    let flag = false
-    while(!verifyName()){
-        alert('הכנס שוב את השם');
-        let fname = document.getElementById('first_name').innerText = "";
-        let lname = document.getElementById('last_name').innerText = "";
-    }
 
     let fname = document.getElementById('first_name').value;
     let lname = document.getElementById('last_name').value;
@@ -77,13 +89,13 @@ function submitReg() {
     let score = 0;
     let actions = [];
 
-    const user = { name: fname + " " + lname, phone: phone, email: email, password: password, score: score, actions: actions};
+    const user = { name: fname + " " + lname, phone: phone, email: email, password: password, score: score, actions: actions };
     console.log(user);
     window.localStorage.setItem(email, JSON.stringify(user));
     document.cookie = `email=${email}; path=/`;
 }
 
 
-function goBegining(){
+function goBegining() {
     location.href = 'index.html';
 }

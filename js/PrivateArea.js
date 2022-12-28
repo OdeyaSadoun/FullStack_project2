366891c452fe7efe60e4e12cf92b54b2f71b5bd0
אודיה סעדון <osadoun@g.jct.ac.il>
2022-12-28 15:46:25 +0200

    table

--- js/PrivateArea.js
+++ js/PrivateArea.js
@@ -6,17 +6,49 @@ function getDetailsForAllUsers() {
 
         // set iteration key name
         var key = localStorage.key(i);
+        console.log(key + "keyyyyyy");
 
         // use key name to retrieve the corresponding value
         var value = localStorage.getItem(key);
+        console.log(value + "value");
 
+        const obj = JSON.parse(value);
         // console.log the iteration key and value
-        str += ('Key: ' + key + ', Value: ' + value);
-        return str;
+        str[i] = obj;
+        console.log(obj.name);
+        console.log(str[i]);
+
     }
+    console.log(str);
+    return str;
 }
 window.onload = function () {
 
-    document.getElementById('tablescore').innerHTML = getDetailsForAllUsers();
+    let table = document.createElement('table');
+    let users = getDetailsForAllUsers();
+    for (let i = 0; i < users.length; i++) {
+        let tr = document.createElement('tr');
+
+        let td1 = document.createElement('td');
+        let td2 = document.createElement('td');
+        let td3 = document.createElement('td');
+
+        let nameUser = document.createTextNode(users[i].name);
+        let scoreUser = document.createTextNode(users[i].score);
+        let lastAction = document.createTextNode("ykhkhkhk");
+
+        td1.appendChild(nameUser);
+        td2.appendChild(scoreUser);
+        td3.appendChild(lastAction);
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+
+        table.appendChild(tr);
+    }
+    document.body.appendChild(table);
 
 }
+
